6468da0a3064380ff4929aea6a350a9cddef47e8
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 13:47:31 +0530

    variabls

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hello, suresh, how are you?"
+echo "suresh:: hi ramesh, i am fine, how are you?"
+echo "ramesh:: i am fine too, how is your work?"
+echo "suresh:: not bad, i am thinking to upgrade devops"
