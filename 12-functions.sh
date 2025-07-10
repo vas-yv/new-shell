af9f698403507ae56ddfa3409e625502d0007b6b
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 15:09:46 +0530

    installtions

---
+++ 12-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2..failure"
+        exit
+    else
+        echo "$2..success"
+    fi        
+
+}
+
+if [ $Userid -ne 0 ]
+then
+    echo "please run the script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
+validate $? "installing mysql"
+
+dnf install gitt -y
+validate $? "installing git"
+
