62de96a933418121c6fa2666744421c3d7bc0f17
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 13:52:06 +0530

    declare variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1:: hello, $person2, how are you?"
+echo "$person2:: hi $person1, i am fine, how are you?"
+echo "$person1:: i am fine too, how is your work?"
+echo "$person2:: not bad, i am thinking to upgrade devops"
