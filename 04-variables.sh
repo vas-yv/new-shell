d230e6650a2452941baec911e97b52f028bdc464
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 13:57:31 +0530

    passing parameters

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,4 +4,4 @@ person1=$1
 person2=$2
 
 echo "$person1:: hi $person2, how are you?"
-echo "$person2:: hi $person1, i am fine you looking so beautiful, love u" 
+echo "$person2:: hi $person1, i am fine you" 
