a09d396b22dc5a3aab6aeb6a4c9a792d20c0f17a
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 14:06:45 +0530

    input from user

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username::"
+ 
+read -s USERNAME  #here username is variable
+
+echo "please enter password"
+
+read -s "PASSWORD"
+
+echo "username is: $USERNAME, password is : $PASSWORD"
