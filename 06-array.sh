544fece41f8fa2ef048c57a7942d8fc0fbf21d25
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 14:13:09 +0530

    passing parameters

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 Movies=("RRR" "Djtillu" "murari")
 
-echo "first movie is:${movie[0]}"
-echo "second movie is:${movie[1]}"
-echo "movies are:${movie[@]}"
+echo "first movie is:${Movies[0]}"
+echo "second movie is:${Movies[1]}"
+echo "movies are:${Movies[@]}"
