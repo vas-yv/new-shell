598c20c0e4fa79bffbc9c976592bddc78d551c8b
vasuy <vasuyaragorla7@gmail.com>
2025-07-10 15:01:45 +0530

    installtions

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Userid=$(id -u)
+
+validate() {
+    echo "exit status: $1"
+    echo "what are you doing: $2"
+
+}
+
+if [ $userid -ne 0 ]
+then
+    echo "please run the script with root access"
+    exit 1
+else
+    echo "your super user"
+fi
+
+dnf install mysql -y
+validate $? "installing mysql"
+
+dnf install git -y
+validate $? "installing git"
