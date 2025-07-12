c7e6d86f89d20eb73393110ce6f52caf241042e1
vasuy <vasuyaragorla7@gmail.com>
2025-07-12 11:16:08 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,18 +2,18 @@
 
 Userid=$(id -u)
 
-validate() {
-    echo "exit status: $1"
-    echo "what are you doing: $2"
+validate(){
+    echo " exit status:$1"
+    echo "what you are doing:$2"
 
 }
 
-if [ $Userid -ne 0 ]
+if [ $userid -ne 0 ]
 then
-    echo "please run the script with root access"
+    echo "please run the user with root access"
     exit 1
 else
-    echo "your super user"
+    echo "you are super user"
 fi
 
 dnf install mysql -y
